5e13c860f197ec6d766ddf8ab20977c16d9c9563
Rob Moore <me@robdmoore.id.au>
2014-07-20 13:32:59 +0800

    Added a test that ensures a connection returned from a transaction is reliable

---
+++ ReliableDbProvider.Tests/TransactionTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using ReliableDbProvider.Tests.Config;
+using ReliableDbProvider.Tests.SqlExpress;
+
+namespace ReliableDbProvider.Tests
+{
+    public class TransactionShould : PooledDbTestBase<SqlExpressProvider>
+    {
+        [Test]
+        public void Return_reliable_connection_when_initiated_from_reliable_connection()
+        {
+            using (var context = GetContext())
+            {
+                using (var transaction = context.Database.Connection.BeginTransaction())
+                {
+                    Assert.That(transaction.Connection, Is.InstanceOf<ReliableSqlDbConnection>());
+                }
+            }
+        }
+    }
+}
