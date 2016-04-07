5c88613e1c4c94a8ab91e68c48eec36f1db8bcdb
Lawrence Short <lawrence.short@careflowconnect.com>
2016-04-07 16:37:54 +0100

    Created override method for enlist transaction that allows the use of a transaction scope.

--- ReliableDbProvider.Tests/TransactionTests.cs
+++ ReliableDbProvider.Tests/TransactionTests.cs
@@ -1,5 +1,7 @@
-﻿using NUnit.Framework;
+﻿using System.Transactions;
+using NUnit.Framework;
 using ReliableDbProvider.Tests.Config;
+using ReliableDbProvider.Tests.Entities;
 using ReliableDbProvider.Tests.SqlExpress;
 
 namespace ReliableDbProvider.Tests
@@ -16,6 +18,24 @@ namespace ReliableDbProvider.Tests
                     Assert.That(transaction.Connection, Is.InstanceOf<ReliableSqlDbConnection>());
                 }
             }
+        }
+
+        [Test]
+        public void Enlist_in_transaction_when_using_a_transaction_scope()
+        {
+            Assert.DoesNotThrow(() =>
+            {
+                using (var context = GetContext())
+                {
+                    using (var transactionScope = new TransactionScope())
+                    {
+                        var user = new User() {Name = "Name"};
+                        context.Users.Add(user);
+                        context.SaveChanges();
+                        transactionScope.Complete();
+                    }
+                }
+            });
         }
     }
 }
